762644f81d651b0fdb04845df33115b52812a8a8
thivatm <thivagartm@gmail.com>
2018-11-26 08:34:38 +0530

    Added calculate function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,15 +13,16 @@ export class HomePage {
 
   countryCodes = [];
   countryNames = [];
+  currencyRate:any;
 
   constructor(public navCtrl: NavController, protected cuService: CurrencyService, public wheelPicker: WheelSelector, public http: HttpClient) {
+    this.calculateCurrency("LKR", "USD", "12");
     this.cuService.getCountries();
-    this.fetchValues();
   }
 
   calculateCurrency(from: String, to:String, amount: String) {
-
-
+    this.currencyRate = this.cuService.getExchangeRate(from, to);
+    console.log("LKR_USD: " + this.cuService.exchangeRate);
   }
   fetchValues() {
     this.http.get("http://data.fixer.io/api/symbols?access_key=36c5ad864460b4395989f11d200fdc5c").subscribe(res => {
--- src/pages/service/cu-service.ts
+++ src/pages/service/cu-service.ts
@@ -8,7 +8,9 @@ export class CurrencyService {
 
     countryCodes = [];
     currencyMap = new Map();
-    constructor(public http: HttpClient) { }
+    exchangeRate: any;
+
+    constructor(public http: HttpClient) {}
 
     getCountries() {
         return this.http.get("http://data.fixer.io/api/symbols?access_key=36c5ad864460b4395989f11d200fdc5c")
@@ -22,12 +24,10 @@ export class CurrencyService {
 
     getExchangeRate(from: String, to: String){
         return this.http.get("http://free.currencyconverterapi.com/api/v5/convert?q=" + from + "_" + to + "&compact=y")
-            .subscribe((exchangeRate) => {
-                
-            })
-    }
+            .subscribe((rate) => {
+                console.log("Rate: " + rate[from + "_" + to].val);
+                this.exchangeRate = rate[from + "_" + to].val;
+            });
+        }
 
-    convertCurrency(){
-        
-    }
 }
