8aa86bf8270cb786fada8c6274238d856271954c
thivatm <thivagartm@gmail.com>
2019-01-08 20:23:49 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,10 +15,13 @@ export class HomePage {
   countryCodes = [];
   countryNames = [];
   resultRate: any;
+  swappedRate: any;
+
   fromValue: any;
   toValue: any;
-  fromCurr: any;
-  toCurr: any;
+  
+  fromCurr: any = 'USD'; // default values
+  toCurr: any = 'LKR'; // default values
 
   constructor(public navCtrl: NavController, protected cuService: CurrencyService, public http: HttpClient) {
     this.fetchCountries();
@@ -43,16 +46,25 @@ export class HomePage {
     let to = this.toCurr;
     try {
       const exchangeRate = await this.cuService.getExchangeRate(from, to);
+      const swapExchangeRate = await this.cuService.getExchangeRate(to, from);
       let rate = exchangeRate[from + "_" + to].val;
+      let swappedRate = swapExchangeRate[to + "_" + from].val;
       this.resultRate = rate;
+      this.swappedRate = swappedRate;
     }
     catch (err) {
       console.error(err);
     }
+    console.log("get exchange executued")
   }
 
   calculateCurrencyOne() {
     this.toValue = this.fromValue * parseFloat(this.resultRate);
+    console.log('Final Value: ' + this.toValue);
+  }
+
+  calculateCurrencyTwo() {
+    this.fromValue = this.toValue * parseFloat(this.swappedRate);
     console.log('Final Value: ' + this.toValue);
   }
 }
